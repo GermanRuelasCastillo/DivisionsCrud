500c197f9c9587ced2022423285432bbe986eb4f
German Ruelas <germanruelas17@gmail.com>
2021-11-06 23:47:11 -0500

    Filters
    pagination done
    filters inside table

--- public/js/app.js
+++ public/js/app.js
@@ -12427,6 +12427,25 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _store__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../store */ "./resources/js/store.js");
 /* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 
 
@@ -12434,55 +12453,132 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var columns = [{
-  title: 'División',
-  dataIndex: 'name',
-  key: 'division'
-}, {
-  title: 'División Superior',
-  dataIndex: ['parent', 'name'],
-  key: 'division-sup'
-}, {
-  title: 'Colaboradores',
-  dataIndex: 'collaborator',
-  key: 'colaboradores'
-}, {
-  title: 'Nivel',
-  dataIndex: 'level',
-  key: 'nivel'
-}, {
-  title: 'Subdivisiones',
-  dataIndex: 'subdivisions',
-  key: 'subdivisiones',
-  render: function render(subdivisions) {
-    return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("p", {
-      children: subdivisions.length
+
+
+function castFilter(array) {
+  var newArray = [];
+  array.forEach(function (element) {
+    newArray.push({
+      text: element,
+      value: element
     });
-  }
-}, {
-  title: 'Embajadores',
-  dataIndex: 'ambassador',
-  key: 'embajadores',
-  render: function render(ambassador) {
-    return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("p", {
-      children: ambassador != null ? ambassador : '-'
-    });
-  }
-}];
+  });
+  return newArray;
+}
 
 function Body() {
+  // FUNCTIONS
   (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
     _store__WEBPACK_IMPORTED_MODULE_2__["default"].dispatch((0,_utils_Http__WEBPACK_IMPORTED_MODULE_1__.getDivisions)());
   }, []);
+  var levels = (0,react_redux__WEBPACK_IMPORTED_MODULE_3__.useSelector)(function (state) {
+    return _toConsumableArray(new Set(state.division.divisions.map(function (d) {
+      return d.level;
+    })));
+  });
+  var divisionsName = (0,react_redux__WEBPACK_IMPORTED_MODULE_3__.useSelector)(function (state) {
+    return _toConsumableArray(new Set(state.division.divisions.map(function (d) {
+      return d.name;
+    })));
+  });
+  var divisionsParentName = (0,react_redux__WEBPACK_IMPORTED_MODULE_3__.useSelector)(function (state) {
+    return _toConsumableArray(new Set(state.division.divisions.map(function (d) {
+      return d.parent.name;
+    })));
+  });
+  var columns = [{
+    title: 'División',
+    dataIndex: 'name',
+    key: 'division',
+    filters: castFilter(divisionsName),
+    onFilter: function onFilter(value, val) {
+      return val.name === value;
+    },
+    sorter: function sorter(a, b) {
+      return a.name - b.name;
+    }
+  }, {
+    title: 'División Superior',
+    dataIndex: ['parent', 'name'],
+    key: 'division-sup',
+    filters: castFilter(divisionsParentName),
+    onFilter: function onFilter(value, val) {
+      return val.parent.name === value;
+    },
+    sorter: function sorter(a, b) {
+      return a.parent.name - b.parent.name;
+    }
+  }, {
+    title: 'Colaboradores',
+    dataIndex: 'collaborator',
+    key: 'colaboradores',
+    onFilter: function onFilter(value, val) {
+      return val.collaborator === value;
+    },
+    sorter: function sorter(a, b) {
+      return a.collaborator - b.collaborator;
+    }
+  }, {
+    title: 'Nivel',
+    dataIndex: 'level',
+    key: 'nivel',
+    filters: castFilter(levels),
+    onFilter: function onFilter(value, val) {
+      return val.level === value;
+    },
+    sorter: function sorter(a, b) {
+      return a.level - b.level;
+    }
+  }, {
+    title: 'Subdivisiones',
+    dataIndex: 'subdivisions',
+    key: 'subdivisiones',
+    render: function render(subdivisions) {
+      return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("p", {
+        children: subdivisions.length
+      });
+    },
+    onFilter: function onFilter(value, val) {
+      return val.subdivisions.length === value;
+    },
+    sorter: function sorter(a, b) {
+      return a.subdivisions.length - b.subdivisions.length;
+    }
+  }, {
+    title: 'Embajadores',
+    dataIndex: 'ambassador',
+    key: 'embajadores',
+    render: function render(ambassador) {
+      return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("p", {
+        children: ambassador != null ? ambassador : '-'
+      });
+    }
+  }];
   var divisions = (0,react_redux__WEBPACK_IMPORTED_MODULE_3__.useSelector)(function (state) {
     return state.division.divisions;
   });
+
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(1),
+      _useState2 = _slicedToArray(_useState, 2),
+      page = _useState2[0],
+      setPage = _useState2[1];
+
+  var _useState3 = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(12),
+      _useState4 = _slicedToArray(_useState3, 2),
+      pageSize = _useState4[0],
+      setPageSize = _useState4[1];
+
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.Fragment, {
     children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(antd__WEBPACK_IMPORTED_MODULE_5__["default"], {
       columns: columns,
       pagination: {
-        current: 1,
-        pageSize: 12
+        divisions: divisions.length,
+        current: page,
+        pageSize: pageSize,
+        onChange: function onChange(page, pageSize) {
+          setPage(page);
+          setPageSize(pageSize);
+        }
       },
       dataSource: divisions,
       bordered: true
--- resources/js/components/Body.js
+++ resources/js/components/Body.js
@@ -1,64 +1,100 @@
-import {useEffect} from 'react';
+import {useEffect,useState} from 'react';
 import { Table } from 'antd';
 import { getDivisions } from '../utils/Http';
 import store from '../store';
 import { useSelector } from 'react-redux';
 
-const columns = [
-    {
-      title: 'División',
-      dataIndex: 'name',
-      key: 'division',
-    },
-    {
-      title: 'División Superior',
-      dataIndex: ['parent', 'name'],
-      key: 'division-sup',
-    },
-    {
-      title: 'Colaboradores',
-      dataIndex: 'collaborator',
-      key: 'colaboradores',
-    },
-    {
-      title: 'Nivel',
-      dataIndex: 'level',
-      key: 'nivel',
-    },
-    {
-      title: 'Subdivisiones',
-      dataIndex: 'subdivisions',
-      key: 'subdivisiones',
-      render: (subdivisions) => {
-        return <p>{subdivisions.length}</p>
-      }
-    },
-    {
-      title: 'Embajadores',
-      dataIndex: 'ambassador',
-      key: 'embajadores',
-      render: (ambassador) => {
-        return <p>{ambassador != null ? ambassador : '-'}</p>
-      }
-    },
-  ];
+
+function castFilter(array) {
+    let newArray = [];
+    array.forEach(element => {
+        newArray.push({
+            text: element,
+            value: element
+        });
+    });
+    return newArray;
+}
 
 function Body() {
+     // FUNCTIONS
+
     useEffect(() => {
         store.dispatch(getDivisions())
     }, []);
+    const levels = useSelector((state) => [...new Set(state.division.divisions.map(d => d.level))]);
+    const divisionsName = useSelector((state) => [...new Set(state.division.divisions.map(d => d.name))]);
+    const divisionsParentName = useSelector((state) => [...new Set(state.division.divisions.map(d => d.parent.name))]);
+    const columns = [
+        {
+          title: 'División',
+          dataIndex: 'name',
+          key: 'division',
+          filters: castFilter(divisionsName),
+          onFilter: (value, val) => val.name === value,
+          sorter: (a, b) => a.name - b.name,
+        },
+        {
+          title: 'División Superior',
+          dataIndex: ['parent', 'name'],
+          key: 'division-sup',
+          filters: castFilter(divisionsParentName),
+          onFilter: (value, val) => val.parent.name === value,
+          sorter: (a, b) => a.parent.name - b.parent.name,
+        },
+        {
+          title: 'Colaboradores',
+          dataIndex: 'collaborator',
+          key: 'colaboradores',
+          onFilter: (value, val) => val.collaborator === value,
+          sorter: (a, b) => a.collaborator - b.collaborator,
+        },
+        {
+          title: 'Nivel',
+          dataIndex: 'level',
+          key: 'nivel',
+          filters: castFilter(levels),
+          onFilter: (value, val) => val.level === value,
+          sorter: (a, b) => a.level - b.level,
+        },
+        {
+          title: 'Subdivisiones',
+          dataIndex: 'subdivisions',
+          key: 'subdivisiones',
+          render: (subdivisions) => {
+            return <p>{subdivisions.length}</p>
+          },
+          onFilter: (value, val) => val.subdivisions.length === value,
+          sorter: (a, b) => a.subdivisions.length - b.subdivisions.length,
+        },
+        {
+          title: 'Embajadores',
+          dataIndex: 'ambassador',
+          key: 'embajadores',
+          render: (ambassador) => {
+            return <p>{ambassador != null ? ambassador : '-'}</p>
+          }
+        },
+    ];
     const divisions = useSelector((state) => state.division.divisions);
+    const [page, setPage] = useState(1);
+    const [pageSize, setPageSize] = useState(12);
     return  (
         <>
             <Table
                 key="divisions"
-                columns={columns}
-                pagination={{ current: 1, pageSize: 12 }}
-                dataSource={divisions}
+                columns={ columns }
+                pagination={{ divisions: divisions.length,current: page, pageSize: pageSize, onChange:(page,pageSize) => {
+                    setPage(page);
+                    setPageSize(pageSize);
+                } }}
+                dataSource = { divisions }
                 bordered
-                />
+            />
         </>
     )
+
+
 }
 
 export default Body;
